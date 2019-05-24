223e917bbc604817bb68ddbc664932ba3781bb18
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-23 23:53:29 -0700

    Update xml_parser.py
    added for loop code but commented it out for now

--- xml_parser.py
+++ xml_parser.py
@@ -6,3 +6,8 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('file.xml')
 #tree = ET.iterparse('file.xml')
 root = tree.getroot()
+
+#for i in root.findall('country/city'):
+#  v = i.get('stuff')
+#  print(v)
+#  root.clear()
