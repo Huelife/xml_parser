97dcdbaaab6bb857196938572ce8be7bd1c33224
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-24 01:49:17 -0700

    Update xml_parser.py
    changed i to record

--- xml_parser.py
+++ xml_parser.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('dataMay-24-2019.xml')
 root = tree.getroot()
 
-for i in root.findall('record'):
-  value = i.find('city').text
+for record in root.findall('record'):
+  value = record.find('city').text
   print(value)
   root.clear()
