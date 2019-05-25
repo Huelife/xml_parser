38f86899f984e03e6259bfb963ad5515fb110c2c
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-24 20:18:11 -0700

    Update xml_parser.py
    added totaling number of cities printed

--- xml_parser.py
+++ xml_parser.py
@@ -6,7 +6,12 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('dataMay-24-2019.xml')
 root = tree.getroot()
 
+city_num = 0
+
 for record in root.findall('record'):
   value = record.find('city').text
   print(value)
+  cit_num += 1
   root.clear()
+  
+print("Total number of cities: {}.".format(city_num))
