832e095b372e98b8eae2c050d3d184f89f080bc5
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-25 18:50:04 -0700

    Update xml_parser.py
    added code to create a new file called information.txt and write the parsed data there

--- xml_parser.py
+++ xml_parser.py
@@ -7,11 +7,17 @@ tree = ET.parse('dataMay-24-2019.xml')
 root = tree.getroot()
 
 city_num = 0
+city_value = ""
 
 #loops through record to find city, print city name, and add total
 for record in root.findall('record'):
   value = record.find('city').text
   print(value)
   city_num += 1
+  city_value += value
+
+#write parsed values to information.txt
+with open('information.txt','w') as info:
+  info.write(city_value)
   
 print("Total number of cities: {}.".format(city_num))
