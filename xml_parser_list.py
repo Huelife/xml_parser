e82f908a38f02f754a4a61c70975fd84c26c0504
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-27 18:29:33 -0700

    Update xml_parser_list.py
    added for loop generator code for list of items and number items on list

--- xml_parser_list.py
+++ xml_parser_list.py
@@ -5,3 +5,14 @@ import xml.etree.ElementTree as ET
 #parsing dataMay-27-2019.xml
 tree = ET.parse('dataMay-27-2019.xml')
 root = tree.getroot()
+
+city_num = 0
+city_value = []
+
+#loops through record to find longitude,latitude,country,city
+for record in root.findall('record'):
+  value = [[record.find('longitude').text,record.find('latitude').text,
+           record.find('country').text,record.find('city').text]]
+  print(value)
+  city_num += 1
+  city_value += value
