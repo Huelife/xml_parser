5f84d531b822df3d0cb8bd6722f7e88f9a2e0cd6
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-27 18:22:55 -0700

    Update xml_parser_list.py
    adding elementtree code to parse dataMay-27-2019.xml

--- xml_parser_list.py
+++ xml_parser_list.py
@@ -1,1 +1,7 @@
 #xml_parser_list.py: Simple XML parser writing list
+
+import xml.etree.ElementTree as ET
+
+#parsing dataMay-27-2019.xml
+tree = ET.parse('dataMay-27-2019.xml')
+root = tree.getroot()
